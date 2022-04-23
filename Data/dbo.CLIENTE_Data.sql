91cb32017963393c90235ff52f17d06b1dce9525
unknown <jesuscanales@outlook.com>
2022-04-23 00:48:03 -0500

    Agregando los registros de la tabla CLIENTE

---
+++ Data/dbo.CLIENTE_Data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[CLIENTE] ([CODCLI], [DNICLI], [NOMCLI], [APECLI], [CELCLI], [EMACLI], [FECNACCLI]) VALUES ('C00001', '78451299', 'Anastasia', 'Campusano Dávila', '997845126', 'acampusano@gmail.com', '1976-06-06')
+INSERT INTO [dbo].[CLIENTE] ([CODCLI], [DNICLI], [NOMCLI], [APECLI], [CELCLI], [EMACLI], [FECNACCLI]) VALUES ('C00002', '78451288', 'Claudio', 'Barrios Guerra', '998822154', 'cbarrios@hotmail.com', '1976-04-11')
+INSERT INTO [dbo].[CLIENTE] ([CODCLI], [DNICLI], [NOMCLI], [APECLI], [CELCLI], [EMACLI], [FECNACCLI]) VALUES ('C00003', '45123691', 'Sofía', 'Paz Solano', '998526314', 'spaz@gmail.com', '1976-08-11')
