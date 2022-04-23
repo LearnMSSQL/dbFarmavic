de667a3dad9b16c8a183689918b44680af42e74b
unknown <jesuscanales@outlook.com>
2022-04-23 01:32:31 -0500

    Agregamos registros a la tabla VENDEDOR

---
+++ Data/dbo.VENDEDOR_Data.sql
@@ -0,0 +1,3 @@
+INSERT INTO [dbo].[VENDEDOR] ([CODVEN], [DNIVEN], [NOMVEN], [APEVEN], [CELVEN], [EMAVEN], [FECNACVEN]) VALUES ('V00001', '98653214', 'María', 'Sánchez Campos', '998521471', 'msanchez@gmail.com', '1976-06-06')
+INSERT INTO [dbo].[VENDEDOR] ([CODVEN], [DNIVEN], [NOMVEN], [APEVEN], [CELVEN], [EMAVEN], [FECNACVEN]) VALUES ('V00002', '42153587', 'Alberto', 'Salas Paz', '958741236', 'asalas@hotmail.com', '1976-04-11')
+INSERT INTO [dbo].[VENDEDOR] ([CODVEN], [DNIVEN], [NOMVEN], [APEVEN], [CELVEN], [EMAVEN], [FECNACVEN]) VALUES ('V00003', '15423587', 'Carlos', 'Ávila Pérez', '963258741', 'cavila@gmail.com', '1976-08-11')
